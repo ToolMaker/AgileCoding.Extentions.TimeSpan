701f7a6c51e0f249d5aa82e32f67bf5c07d47ccc
Ernie <gunning.ernie@gmail.com>
2021-06-01 14:27:43 +0200

    Project Start

---
+++ AgileCoding.Extentions.TimeSpan/TimeSpanExtentions.cs
@@ -0,0 +1,31 @@
+﻿namespace AgileCoding.Extentions.TimeSpans
+{
+    using System;
+    using AgileCoding.Extentions.Activators;
+
+    public static class TimeSpanExtentions
+    {
+        public static TimeSpan ToTimeSpan(this string stringValue)
+        {
+            TimeSpan parseResult;
+            TimeSpan.TryParse(stringValue, out parseResult);
+            return parseResult;
+        }
+
+        public static TimeSpan ToTimeSpan<IExceptionType>(this string stringValue, string errorMessage = null)
+             where IExceptionType : Exception, new()
+        {
+            string tempErrorMessage = errorMessage == null
+                ? $"Value provided '{stringValue}' is not a valid TimeSpan"
+                : errorMessage;
+
+            TimeSpan parseResult;
+            if (!TimeSpan.TryParse(stringValue, out parseResult))
+            {
+                throw typeof(IExceptionType).CreateInstanceWithoutLogging<IExceptionType>(tempErrorMessage);
+            }
+
+            return parseResult;
+        }
+    }
+}
---
+++ ReadMePage.md
@@ -0,0 +1,5 @@
+﻿#AgileCoding.Extentions.TimeSpan
+
+###Extenions related to TimeSpan
+
+1.	TimeSpanExtentions - Static class containing TimeSpan extentions
